85eb7de4e64f1f9b12ed5b36b1409bce9e3c7690
Voosk <milkgolium@gmail.com>
2023-09-23 22:32:55 +0800

    an commit

--- git.ps1
+++ git.ps1
@@ -1,6 +1,11 @@
 # git init
-git add .
+#git add .
+#git commit -m "an commit"
+#git branch -M main
+#git remote add origin https://github.com/MilkGolium/EOP-web.git
+#git push -u origin main
+
+git add.
 git commit -m "an commit"
 git branch -M main
-git remote add origin https://github.com/MilkGolium/EOP-web.git
 git push -u origin main
